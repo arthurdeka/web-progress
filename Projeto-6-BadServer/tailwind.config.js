d2e5126ba139567c575f0f6a4353598c8732b573
Arthur DK <arthurdk01@gmail.com>
2023-06-05 21:21:43 -0300

    Project 6 added

---
+++ Projeto-6-BadServer/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html", "./build/js/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        purpleCustom: {
+          light: '#d7adff',
+          DEFAULT: '#8300ff',
+          dark: '#3b0073',
+        }
+      },
+      screens: {
+        'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
+        'tallscreen': { 'raw': '(max-aspect-ratio: 13/20)' },
+      },
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'scaleY(0)' },
+          '80%': { transform: 'scaleY(1.2)' },
+          '100%': { transform: 'scaleY(1)' },
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+      }
+    },
+  },
+  plugins: [],
+}
